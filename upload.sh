f39abcd751d0fe34cf322b55e4ad2b4772426bdf
Islabre <mizuemxc@anonaddy.me>
2026-07-21 20:16:15 +0700

    Adding favicon and upload script
    Credit to https://realfavicongenerator.net/ for free favicon generator

---
+++ upload.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu
+
+artifact="dist"
+remote_host="root@personal-server"
+name="tempchat"
+dest="/var/www/html/$name"
+
+if [ -d "$artifact" ]; then
+    echo "Cleaning up old artifact."
+    rm -r "$artifact"
+fi
+
+echo "Building website."
+npm run build
+
+echo "Syncing content to remote server."
+rsync -av --delete "$artifact" "$remote_host:$dest"
+
+echo "Successfully uploaded."
