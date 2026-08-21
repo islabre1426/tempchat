f3e90e1d52fee7130fd5243145aac63ab5d3cc9c
Islabre <islabre1426@gmail.com>
2026-08-21 10:26:01 +0700

    Initial Cloudflare TURN server support

--- src/js/components/util.js
+++ src/js/components/util.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export function formatBytes(bytes, decimals = 1) {
     if (bytes === 0) return '0 Bytes';
 
@@ -9,4 +11,27 @@ export function formatBytes(bytes, decimals = 1) {
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+export async function generateCloudflareTurn() {
+    const TURN_TOKEN_ID = process.env.CLOUDFLARE_TURN_TOKEN_ID;
+    const TURN_API_TOKEN = process.env.CLOUDFLARE_TURN_API_TOKEN;
+    const TURN_GENERATOR_URL = `https://rtc.live.cloudflare.com/v1/turn/keys/${TURN_TOKEN_ID}/credentials/generate-ice-servers`;
+
+    try {
+        const response = await fetch(TURN_GENERATOR_URL, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${TURN_API_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'ttl': 86400,
+            }),
+        });
+
+        return await response.json();
+    } catch (err) {
+        throw err;
+    }
 }
